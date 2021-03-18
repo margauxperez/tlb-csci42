cc6650712f1fecebbe50eaff89439f4a41233b00
Kyla Nicole Villegas <kyla.villegas@obf.ateneo.edu>
2021-03-18 22:10:41 +0800

    fix: update feedback Firebase connection

--- feedback.js
+++ feedback.js
@@ -1,11 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCu5IEJ60FeswJ1eqBGU2obLZW1wEwa2j8",
-    authDomain: "the-learning-buddy-96ede.firebaseapp.com",
-    projectId: "the-learning-buddy-96ede",
-    storageBucket: "the-learning-buddy-96ede.appspot.com",
-    messagingSenderId: "756080195049",
-    appId: "1:756080195049:web:be239027c9c384ca1c0778"
-};
+    apiKey: "AIzaSyBTPfcNxrT2RJxbCmkladp0ntQEh2UuONs",
+    authDomain: "the-learning-buddy-66812.firebaseapp.com",
+    databaseURL: "https://the-learning-buddy-66812-default-rtdb.firebaseio.com",
+    projectId: "the-learning-buddy-66812",
+    storageBucket: "the-learning-buddy-66812.appspot.com",
+    messagingSenderId: "835249560127",
+    appId: "1:835249560127:web:36b8a29df26129227d768a"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
