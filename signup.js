09683e1d7d1971c053d63ad5fcc451ab0dc6ca86
quielledelarosa <quielle.delarosa@obf.ateneo.edu>
2021-03-19 03:36:42 -0400

    feat: returns an error if no email/password is inputted

--- signup.js
+++ signup.js
@@ -1,39 +1,79 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyBTPfcNxrT2RJxbCmkladp0ntQEh2UuONs",
-    authDomain: "the-learning-buddy-66812.firebaseapp.com",
-    projectId: "the-learning-buddy-66812",
-    storageBucket: "the-learning-buddy-66812.appspot.com",
-    messagingSenderId: "835249560127",
-    appId: "1:835249560127:web:36b8a29df26129227d768a"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBTPfcNxrT2RJxbCmkladp0ntQEh2UuONs",
+  authDomain: "the-learning-buddy-66812.firebaseapp.com",
+  projectId: "the-learning-buddy-66812",
+  storageBucket: "the-learning-buddy-66812.appspot.com",
+  messagingSenderId: "835249560127",
+  appId: "1:835249560127:web:36b8a29df26129227d768a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-   const auth = firebase.auth();
+const auth = firebase.auth();
 
-   function signUp(){
+function signUp() {
 
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+  function ValidateEmail(email)
+{
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if(inputText.value.match(mailformat))
+{
 
-    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
-    promise.catch(e => alert(e.message));
-    alert("Signed Up")
-   }
+const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+promise.catch(e => alert(e.message));
 
-   function signIn(){
+alert("Signed Up");
+
+window.location.href = "index.html";
+
+}     
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
+  
+//   if (email.value === '' || email.value === null || password.value === '' || password.value === null) {
     
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
+//     alert("Error: Please Input Email/Password");
+  
 
-    const promise = auth.signInWithEmailAndPassword(email.value, password.value);
-    promise.catch(e => alert(e.message));
-    alert("Signed In " + email.value);
+// } else {
 
-   }
+//     const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+//     promise.catch(e => alert(e.message));
 
-   function signOut(){
-       auth.signOut();
-       alert("Signed Out");
-   }
+//     alert("Signed Up");
+
+//     window.location.href = "index.html";
+//   }
+// }
+
+function signIn() {
+
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+
+
+  if (email.value === '' || email.value === null || password.value === '' || password.value === null){
+
+    alert("Error Please Input Email/Password");
+  }else{
+
+  const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+  promise.catch(e => alert(e.message));
+  alert("Signed In " + email.value);
+
+   window.location.href = "index.html";
+  }
+}
+
+function signOut() {
+  auth.signOut();
+  alert("Signed Out");
+}
